05b2d47b574a09ab57a44220ce52094c73a1588c
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2021-02-04 17:00:24 -0800

    Update tuffy-moon.tsx

--- components/illustrations/tuffy-moon.tsx
+++ components/illustrations/tuffy-moon.tsx
@@ -6,11 +6,16 @@ export default function TuffyCamper(props: Illustration['props']) {
       <svg
         width={props.width}
         height={props.height}
-        viewBox="0 0 254 254"
+        viewBox="-4 -4 265 265"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <circle cx={127} cy={127} r={127} fill="#F4F1C9" />
+        <circle 
+          cx={127} cy={127} r={127} 
+          fill="#F4F1C9"
+          stroke="#513513"
+          strokeWidth={7.522}
+        />
       </svg>
     </Illustration>
   );
