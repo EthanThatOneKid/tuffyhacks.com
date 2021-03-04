ae77bbdd993c057f4b88480d443962409570c776
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2021-03-03 18:13:17 -0800

    Update ambient.d.ts

--- ambient.d.ts
+++ ambient.d.ts
@@ -1,1 +1,6 @@
+/**
+ * This module declaration is necessary because the
+ * 'react-twitch-embed' NPM package does not currently
+ * support TypeScript (as of v2.1.0).
+ */
 declare module 'react-twitch-embed';
