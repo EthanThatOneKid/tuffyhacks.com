de9b0fa3a2d13d87d22f434831e2029c3fbee27c
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2021-03-11 21:39:15 -0800

    Added `/schedules` redirect.

--- next.config.js
+++ next.config.js
@@ -1,28 +1,8 @@
-/**
- * Copyright 2020 Vercel Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+const EXTERNAL_SCHEDULE_URL =
+  'https://www.notion.so/440f7327a62a48ceb15c22fc1c445483?v=63d1c6fb62004b3d904654f4b9991287';
 
 module.exports = {
-  images: {
-    domains: [
-      'www.datocms-assets.com',
-      'a.storyblok.com',
-      'images.ctfassets.net',
-      'images.prismic.io',
-      'cdn.aglty.io'
-    ],
-    imageSizes: [24, 64, 300]
-  }
+  redirects: async () => [
+    { source: '/schedule', destination: EXTERNAL_SCHEDULE_URL, permanent: true }
+  ]
 };
