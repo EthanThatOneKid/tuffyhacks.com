eaf83b839612180b4bf48a61c2ea696cde4d8505
Ethan <ethan.r.davidson@gmail.com>
2021-03-13 15:29:01 -0800

    Implemented sponsors

--- components/sponsors-section-2021.tsx
+++ components/sponsors-section-2021.tsx
@@ -5,7 +5,13 @@ import Trailer from './illustrations/tuffy-trailer';
 import styles from './sponsors-section-2021.module.css';
 import { LEAF_COLORS } from '@lib/constants';
 
-const sponsors: { label: string; url: string; picture: string }[] = [
+interface CompanyLogo {
+  label: string;
+  url: string;
+  picture: string;
+}
+
+const sponsors: CompanyLogo[] = [
   { label: 'Codecademy', url: 'https://codecademy.com/', picture: '/sponsors/codecademy.png' },
   { label: 'ECS-ICC', url: 'https://ecsicc.org/', picture: '/sponsors/ecs.png' },
   { label: 'Echo AR', url: 'https://echoar.xyz/', picture: '/sponsors/echoar.png' },
@@ -18,8 +24,33 @@ const sponsors: { label: string; url: string; picture: string }[] = [
   { label: 'Linode', url: 'https://linode.com/', picture: '/sponsors/linode.png' }
 ];
 
-const partners: { label: string; url: string; picture: string }[] = [
-  { label: 'ACM CSUF', url: 'https://acmcsuf.com/', picture: '/partners/acmcsuf.png' }
+const partners: CompanyLogo[] = [
+  { label: 'ACM at CSUF', url: 'https://acmcsuf.com/', picture: '/partners/acm-csuf.png' },
+  {
+    label: 'ACM Create at CSUF',
+    url: 'https://acmcsuf.com/paths/#create',
+    picture: '/partners/acm-create.png'
+  },
+  {
+    label: 'ACM Dev at CSUF',
+    url: 'https://acmcsuf.com/paths/#dev',
+    picture: '/partners/acm-dev.png'
+  },
+  {
+    label: 'SHPE at CSUF',
+    url: '#', // TODO: Find appropriate URL.
+    picture: '/partners/shpe.png'
+  },
+  {
+    label: 'DSC at CSUF',
+    url: 'http://dsc-csuf.com/',
+    picture: '/partners/dsc.png'
+  },
+  {
+    label: 'Theta Tau at CSUF',
+    url: '#', // TODO: Find appropriate URL.
+    picture: '/partners/csuf-theta-tau.png'
+  }
 ];
 
 export default function SponsorsSection2021() {
@@ -33,14 +64,17 @@ export default function SponsorsSection2021() {
       </IllustrationContainer>
       <div className={cn(styles['info-container'])}>
         <h2>Sponsors</h2>
-        <div className={cn(styles['sponsors-container'])}>
+        <div className={cn(styles['logos-container'])}>
           {sponsors.map(({ label, url, picture }) => (
             <a href={url} key={label}>
               <img src={picture} alt={label} />
             </a>
           ))}
         </div>
-        <div className={cn(styles['partners-container'])}>
+      </div>
+      <div className={cn(styles['info-container'])}>
+        <h2>Partners</h2>
+        <div className={cn(styles['logos-container'])}>
           {partners.map(({ label, url, picture }) => (
             <a href={url} key={label}>
               <img src={picture} alt={label} />
