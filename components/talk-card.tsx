a9ffc5f43ef03c45d8b4719347cf9f5fb9824762
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2021-03-22 19:52:34 -0700

    Update talk-card.tsx

--- components/talk-card.tsx
+++ components/talk-card.tsx
@@ -1,25 +1,8 @@
-/**
- * Copyright 2020 Vercel Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import cn from 'classnames';
 import Image from 'next/image';
-import Link from 'next/link';
 import { useState, useEffect } from 'react';
 import { parseISO, format, isBefore, isAfter } from 'date-fns';
-import { Talk } from '@lib/types';
+import { Speaker, Talk } from '@lib/types';
 import styles from './talk-card.module.css';
 
 type Props = {
@@ -58,29 +41,33 @@ export default function TalkCard({ talk: { title, speaker, start, end }, showTim
           <h4 title={title} className={styles.title}>
             {title}
           </h4>
-          <div className={styles.speaker}>
-            <div className={styles['avatar-group']}>
-              {speaker.map(s => (
-                <div key={s.name} className={styles['avatar-wrapper']}>
-                  <Image
-                    loading="lazy"
-                    alt={s.name}
-                    className={styles.avatar}
-                    src={s.image.url}
-                    title={s.name}
-                    width={24}
-                    height={24}
-                  />
-                </div>
-              ))}
-            </div>
-            <h5 className={styles.name}>
-              {speaker.length === 1 ? speaker[0].name : `${speaker.length} speakers`}
-            </h5>
-          </div>
+          {/* <Speaker /> */}
         </div>
       </a>
       {/* </Link> */}
     </div>
   );
 }
+
+const SpeakerDetails = ({ speaker }: { speaker: Speaker[] }) => (
+  <div className={styles.speaker}>
+    <div className={styles['avatar-group']}>
+      {speaker.map(s => (
+        <div key={s.name} className={styles['avatar-wrapper']}>
+          <Image
+            loading="lazy"
+            alt={s.name}
+            className={styles.avatar}
+            src={s.image.url}
+            title={s.name}
+            width={24}
+            height={24}
+          />
+        </div>
+      ))}
+    </div>
+    <h5 className={styles.name}>
+      {speaker.length === 1 ? speaker[0].name : `${speaker.length} speakers`}
+    </h5>
+  </div>
+);
